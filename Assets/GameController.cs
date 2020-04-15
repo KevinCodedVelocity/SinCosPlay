554e05a754f784d9f7627c46aad8de68ca034ad3
Kevin Howard <kevinh@codedvelocity.com>
2020-04-14 21:15:57 -0500

    initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    const float xyOffset = 2;
+
+    public GameObject target;
+
+    const int size = 50;
+    public GameObject[,] array = new GameObject[size,size]; 
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                array[x, y] = (GameObject)Instantiate(target, new Vector3(x*3, 0, y*3), Quaternion.identity);
+            }
+        }
+
+        target.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        const float height = 3;
+        const float offsetRate = 0.1f;
+
+        float cameraTime = Time.time / 8;
+        Camera.main.transform.LookAt(new Vector3(size / 2, 0, size / 2));
+        Camera.main.transform.position = new Vector3(
+            size/2 * Mathf.Cos(cameraTime), 
+            height + 1 + height * Mathf.Sin(Time.time/2), 
+            size/2 * Mathf.Sin(cameraTime));
+
+        for (int x = 0; x < size; ++x)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                if (array[x, y] != null)
+                {
+                    float offset = (x * offsetRate) + (y * offsetRate) * size * offsetRate;
+                    array[x, y].transform.position = new Vector3(
+                        x * xyOffset,
+                        height * Mathf.Sin(Time.time + offset),
+                        y * xyOffset);
+
+                    array[x, y].GetComponent<MeshRenderer>().material.color = 
+                        new Color(
+                            Mathf.Sin(Time.time + offset),
+                            0, Mathf.Cos(Time.time + offset),
+                            Mathf.Sin(Time.time/2 + offset)
+                            );
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Gradient.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Gradient : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var startColor = Color.red;
+        //var endColor = Color.blue;
+
+        //var mesh = GetComponent<MeshFilter>().mesh;
+        //var colors = new Color[mesh.vertices.Length];
+        //colors[0] = startColor;
+        //colors[1] = endColor;
+        //colors[2] = startColor;
+        //colors[3] = endColor;
+        //mesh.colors = colors;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var color1 = new Color(0, SinPositive(Time.time), 0);
+        var color2 = new Color(0, SinPositive(Time.time), CosPositive(Time.time));
+        var color3 = new Color(0, CosPositive(Time.time), 0);
+        var color4 = new Color(0, CosPositive(Time.time), SinPositive(Time.time));
+
+        var mesh = GetComponent<MeshFilter>().mesh;
+        var colors = new Color[mesh.vertices.Length];
+        colors[0] = color1;
+        colors[1] = color2;
+        colors[2] = color3;
+        colors[3] = color4;
+
+        mesh.colors = colors;
+
+    }
+
+    float CosPositive(float input)
+    {
+        return (1 + Mathf.Cos(input)) / 2;
+    }
+
+    float SinPositive(float input)
+    {
+        return (1 + Mathf.Sin(input))/2;
+    }
+}
